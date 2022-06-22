34bfd541db9d167ff8fede54c16807ac52028dc1
Coleman <charleebrown22@gmail.com>
2022-06-22 09:03:07 -0500

    adding webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
-webpack.config.js
-
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
-	
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-};
+		entry:{
+			connection: './routes/configs.js',
+		indexed: './routes/index.js',
+		users:'./routes/users.js',
+		},
+			
+		output: {
+			filename: 'main.js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		module: {
+			rules: [{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}]
+		}
+	};
